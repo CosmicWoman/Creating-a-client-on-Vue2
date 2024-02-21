6e2fc4aa8622b8c6ec2dd4beaedf7f9138fa358b
Cosmic Woman <mikaat2r1370@gmail.com>
2024-02-21 19:23:20 +0300

    Готова форма создания клиента. Без разбивки на компоненты.(

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <NewClient/>
+</template>
+
+<script>
+import NewClient from "@/components/NewClient";
+
+export default {
+  components: {NewClient }
+}
+</script>
+
+<style scoped lang="sass" src="./style/inputStyle.sass"></style>
